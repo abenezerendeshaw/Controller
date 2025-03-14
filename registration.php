05bfa4b0214ddb23b296ba7f81f8e52b99e376a9
Envair <47439360+abenezerendeshaw@users.noreply.github.com>
2025-03-14 20:35:38 +0300

    Intial commit

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Hululabs_Helloworld',
+    'Hululabs_Helloworld_controller',
     __DIR__
 );
