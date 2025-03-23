db1e23576e6df7d9abd93bd01ec60156f4c89a7c
Envair <47439360+abenezerendeshaw@users.noreply.github.com>
2025-03-23 17:46:07 +0300

    Update Index.php

--- Controller/index/Index.php
+++ Controller/index/Index.php
@@ -1,5 +1,5 @@
 <?php
-namespace HuluLabs\HelloWorldController\Controller\Index;
+namespace HuluLabs\HelloWorldController\Controller\index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
